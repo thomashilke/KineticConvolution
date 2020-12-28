2bd7c0087701aba0d6c3f20b22f03014060ca806
Thomas Hilke <thomas@hilke.ch>
2020-12-28 22:59:09 +0100

    Move meat of convolution to a static helper class

--- src/KineticConvolution/Convolution.cs
+++ src/KineticConvolution/Convolution.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using static KineticConvolution.Algorithms;
+using static KineticConvolution.ConvolutionHelper;
 
 namespace KineticConvolution
 {
@@ -45,70 +45,5 @@ namespace KineticConvolution
         public IEnumerable<Tracing> Shape2 { get; }
 
         public IEnumerable<ConvolvedTracing> ConvolvedTracings { get; }
-
-        private IEnumerable<ConvolvedTracing> Convolve(Tracing tracing1, Tracing tracing2) =>
-            (tracing1, tracing2) switch
-            {
-                (Arc arc1, Arc arc2) => ConvolveArcs(arc1, arc2),
-                (Arc arc, Segment segment) => ConvolveArcAndSegment(arc, segment),
-                (Segment segment, Arc arc) => ConvolveArcAndSegment(arc, segment),
-                (Segment _, Segment _) => Enumerable.Empty<ConvolvedTracing>(),
-                _ => throw new NotSupportedException()
-            };
-
-        private static IEnumerable<ConvolvedTracing> ConvolveArcs(Arc arc1, Arc arc2)
-        {
-            if (arc1.Directions.Orientation == arc2.Directions.Orientation)
-            {
-                return Intersection(arc1.Directions, arc2.Directions)
-                    .Select(range =>
-                            new Arc(1, Sum(arc1.Center, arc2.Center), range, arc1.Radius.Add(arc2.Radius)))
-                    .Select(arc => new ConvolvedTracing(arc, arc1, arc2));
-            }
-            else
-            {
-                return Intersection(arc1.Directions, arc2.Directions.Opposite())
-                    .Select(range =>
-                            new Arc(1, Sum(arc1.Center, arc2.Center), range, arc1.Radius.Substract(arc2.Radius)))
-                    .Select(arc => new ConvolvedTracing(arc, arc1, arc2));
-            }
-        }
-
-        private static IEnumerable<ConvolvedTracing> ConvolveArcAndSegment(Arc arc, Segment segment)
-        {
-            switch (arc.Directions.Orientation)
-            {
-                case Orientation.CounterClockwise:
-                    return segment.NormalDirection().Opposite().BelongsTo(arc.Directions)
-                        ? new [] { new ConvolvedTracing(
-                            new Segment(
-                                1,
-                                Sum(
-                                    segment.Start,
-                                    arc.Center.Translate(segment.NormalDirection().Opposite(), arc.Radius)),
-                                Sum(
-                                    segment.End,
-                                    arc.Center.Translate(segment.NormalDirection().Opposite(), arc.Radius))),
-                            arc, segment) }
-                        : Enumerable.Empty<ConvolvedTracing>();
-
-                case Orientation.Clockwise:
-                    return segment.NormalDirection().BelongsTo(arc.Directions)
-                        ? new [] { new ConvolvedTracing(
-                            new Segment(
-                                1,
-                                Sum(
-                                    segment.Start,
-                                    arc.Center.Translate(segment.NormalDirection(), arc.Radius)),
-                                Sum(
-                                    segment.End,
-                                    arc.Center.Translate(segment.NormalDirection(), arc.Radius))),
-                            arc, segment) }
-                        : Enumerable.Empty<ConvolvedTracing>();
-
-                default:
-                    throw new NotSupportedException();
-            }
-        }
     }
 }
