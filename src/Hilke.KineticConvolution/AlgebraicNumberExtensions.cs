a601e9625ecc4146aefbb730dcebabac48154c3b
Xavier Hahn <xavier.hahn@gmail.com>
2021-01-13 08:09:56 +0100

    Add validation for parameters of extension methods

--- src/Hilke.KineticConvolution/AlgebraicNumberExtensions.cs
+++ src/Hilke.KineticConvolution/AlgebraicNumberExtensions.cs
@@ -1,25 +1,62 @@
+using System;
+
 namespace Hilke.KineticConvolution
 {
     public static class AlgebraicNumberExtensions
     {
         public static bool IsZero<TAlgebraicNumber>(this IAlgebraicNumber<TAlgebraicNumber> number)
-            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber> =>
-            number.Sign() == 0;
+            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber>
+        {
+            if (number == null)
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            return number.Sign() == 0;
+        }
 
         public static bool IsStrictlyPositive<TAlgebraicNumber>(this IAlgebraicNumber<TAlgebraicNumber> number)
-            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber> =>
-            number.Sign() == 1;
+            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber>
+        {
+            if (number == null)
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            return number.Sign() == 1;
+        }
 
         public static bool IsPositive<TAlgebraicNumber>(this IAlgebraicNumber<TAlgebraicNumber> number)
-            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber> =>
-            number.Sign() >= 0;
+            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber>
+        {
+            if (number == null)
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            return number.Sign() >= 0;
+        }
 
         public static bool IsStrictlyNegative<TAlgebraicNumber>(this IAlgebraicNumber<TAlgebraicNumber> number)
-            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber> =>
-            number.Sign() == -1;
+            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber>
+        {
+            if (number == null)
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            return number.Sign() == -1;
+        }
 
         public static bool IsNegative<TAlgebraicNumber>(this IAlgebraicNumber<TAlgebraicNumber> number)
-            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber> =>
-            number.Sign() <= 0;
+            where TAlgebraicNumber : IAlgebraicNumber<TAlgebraicNumber>
+        {
+            if (number == null)
+            {
+                throw new ArgumentNullException(nameof(number));
+            }
+
+            return number.Sign() <= 0;
+        }
     }
 }
