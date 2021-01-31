dd6caa84193b11a98616ba1b146b8c7b8d096c34
Admir Hasanovic <admir.hasanovic.dev@gmail.com>
2021-01-31 21:52:16 +0100

    Add more drawings

--- tests/Hilke.KineticConvolution.Tests/ConvolutionFactoryTests.cs
+++ tests/Hilke.KineticConvolution.Tests/ConvolutionFactoryTests.cs
@@ -194,5 +194,50 @@ namespace Hilke.KineticConvolution.Tests
             actual.Should().HaveCount(1);
             actual.Single().Convolution.As<Segment<double>>().Should().BeEquivalentTo(expected);
         }
+
+        [Test]
+        public void Test()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 1,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 3.0,
+                weight: 1,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 1.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.Clockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+            var expected = _factory.CreateArc(
+                weight: 1,
+                radius: -1,
+                centerX: 6,
+                centerY: 6,
+                directionStartX: -12,
+                directionStartY: 0,
+                directionEndX: 0,
+                directionEndY: -3,
+                orientation: Orientation.CounterClockwise);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Segment<double>>().Should().BeEquivalentTo(expected);
+        }
     }
 }
