166dcd7f5858ebf58d51c1923525a981b900644d
Admir Hasanovic <admir.hasanovic.dev@gmail.com>
2021-02-01 13:42:01 +0100

    Implement all test cases define in the READMe file

--- tests/Hilke.KineticConvolution.Tests/ConvolutionFactoryTests.cs
+++ tests/Hilke.KineticConvolution.Tests/ConvolutionFactoryTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Linq;
 
 using NUnit.Framework;
+
 using FluentAssertions;
 
 namespace Hilke.KineticConvolution.Tests
@@ -17,13 +18,15 @@ namespace Hilke.KineticConvolution.Tests
             _factory = new ConvolutionFactory<double>(calculator);
         }
 
+        // Case 1
         [Test]
-        public void When_calling_ConvolveArcs_With_same_counterClockwise_orientation_Then_the_correct_result_Should_be_returned()
+        public void
+            When_calling_ConvolveArcAndSegment_With_two_shapes_without_tangents_Then_the_result_Should_be_empty()    
         {
             // Arrange arc1
-            var arc1 = _factory.CreateArc(
+            var arc = _factory.CreateArc(
                 radius: 2.0,
-                weight: 1,
+                weight: 3,
                 centerX: 1.0,
                 centerY: 3.0,
                 directionStartX: 2.0,
@@ -32,139 +35,30 @@ namespace Hilke.KineticConvolution.Tests
                 directionEndY: 5.0,
                 orientation: Orientation.CounterClockwise);
 
-            // Arrange arc2
-            var arc2 = _factory.CreateArc(
-                radius: 1.0,
-                weight: 2,
-                centerX: 5.0,
-                centerY: 3.0,
-                directionStartX: 1.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 3.0,
-                orientation: Orientation.CounterClockwise);
-
-            // Arrange expected
-            var expected = _factory.CreateArc(
-                radius: 3.0,
-                weight: 2,
-                centerX: 6.0,
-                centerY: 6.0,
-                directionStartX: 12.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 51.0,
-                orientation: Orientation.CounterClockwise);
+            // Arrange segment
+            var segment = _factory.CreateSegment(
+                startX: 10,
+                startY: 5,
+                endX: 15,
+                endY: 10,
+                weight: 4);
 
             // Act
-            var actual = _factory.ConvolveArcs(arc1, arc2);
+            var actual = _factory.ConvolveArcAndSegment(arc, segment);
 
             // Assert
-            actual.Should().HaveCount(1);
-            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+            actual.Should().BeEmpty();
         }
 
+        // Case 2
         [Test]
-        public void When_calling_ConvolveArcs_With_opposite_orientations_Then_the_correct_result_Should_be_returned()
-        {
-            // Arrange arc1
-            var arc1 = _factory.CreateArc(
-                radius: 2.0,
-                weight: 1,
-                centerX: 1.0,
-                centerY: 3.0,
-                directionStartX: 2.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 5.0,
-                orientation: Orientation.CounterClockwise);
-
-            // Arrange arc2
-            var arc2 = _factory.CreateArc(
-                radius: 1.0,
-                weight: 2,
-                centerX: 5.0,
-                centerY: 3.0,
-                directionStartX: 1.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 3.0,
-                orientation: Orientation.Clockwise);
-
-            // Arrange expected
-            var expected = _factory.CreateArc(
-                radius: 1.0,
-                weight: 2,
-                centerX: 6.0,
-                centerY: 6.0,
-                directionStartX: 12.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 31.0,
-                orientation: Orientation.CounterClockwise);
-
-            // Act
-            var actual = _factory.ConvolveArcs(arc1, arc2);
-
-            // Assert
-            actual.Should().HaveCount(1);
-            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
-        }
-
-        [Test]
-        public void When_calling_ConvolveArcs_With_same_clockwise_orientation_Then_the_correct_result_Should_be_returned()
-        {
-            // Arrange arc1
-            var arc1 = _factory.CreateArc(
-                radius: 2.0,
-                weight: 1,
-                centerX: 1.0,
-                centerY: 3.0,
-                directionStartX: 2.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 5.0,
-                orientation: Orientation.Clockwise);
-
-            // Arrange arc2
-            var arc2 = _factory.CreateArc(
-                radius: 1.0,
-                weight: 2,
-                centerX: 5.0,
-                centerY: 3.0,
-                directionStartX: 1.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 3.0,
-                orientation: Orientation.Clockwise);
-
-            // Arrange expected
-            var expected = _factory.CreateArc(
-                radius: 3.0,
-                weight: 2,
-                centerX: 6.0,
-                centerY: 6.0,
-                directionStartX: 12.0,
-                directionStartY: 0.0,
-                directionEndX: 0.0,
-                directionEndY: 51.0,
-                orientation: Orientation.Clockwise);
-
-            // Act
-            var actual = _factory.ConvolveArcs(arc1, arc2);
-
-            // Assert
-            actual.Should().HaveCount(1);
-            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
-        }
-
-        [Test]
-        public void When_calling_ConvolveArcAndSegment_With_same_counterClockwise_orientation_Then_the_correct_result_Should_be_returned()
+        public void
+            When_calling_ConvolveArcAndSegment_With_same_counterClockwise_orientation_Then_the_correct_result_Should_be_returned()
         {
             // Arrange arc
             var arc = _factory.CreateArc(
                 radius: 2.0,
-                weight: 1,
+                weight: 5,
                 centerX: 1.0,
                 centerY: 3.0,
                 directionStartX: 2.0,
@@ -188,15 +82,66 @@ namespace Hilke.KineticConvolution.Tests
                 startY: 8,
                 endX: 13,
                 endY: 13,
-                weight: 4);
+                weight: 20);
 
             // Assert
             actual.Should().HaveCount(1);
             actual.Single().Convolution.As<Segment<double>>().Should().BeEquivalentTo(expected);
         }
 
+        // Case 3
         [Test]
-        public void Test()
+        public void
+            When_calling_ConvolveArcs_With_same_counterClockwise_orientation_and_r1_greater_than_r2_Then_the_correct_result_Should_be_returned()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 2,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 1.0,
+                weight: 2,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 1.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange expected
+            var expected = _factory.CreateArc(
+                radius: 3.0,
+                weight: 4,
+                centerX: 6.0,
+                centerY: 6.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 51.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 4
+        [Test]
+        public void
+            When_calling_ConvolveArcs_With_different_orientation_and_r1_smaller_than_r2_Then_the_correct_result_Should_be_returned()
         {
             // Arrange arc1
             var arc1 = _factory.CreateArc(
@@ -222,11 +167,10 @@ namespace Hilke.KineticConvolution.Tests
                 directionEndY: 3.0,
                 orientation: Orientation.Clockwise);
 
-            // Act
-            var actual = _factory.ConvolveArcs(arc1, arc2);
+            // Arrange expected
             var expected = _factory.CreateArc(
-                weight: 1,
                 radius: -1,
+                weight: 1,
                 centerX: 6,
                 centerY: 6,
                 directionStartX: -12,
@@ -235,9 +179,262 @@ namespace Hilke.KineticConvolution.Tests
                 directionEndY: -3,
                 orientation: Orientation.CounterClockwise);
 
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
             // Assert
             actual.Should().HaveCount(1);
             actual.Single().Convolution.As<Segment<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 5
+        [Test]
+        public void
+            When_calling_ConvolveArcs_With_opposite_orientations_and_r1_greater_than_r2_Then_the_correct_result_Should_be_returned()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 1,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 1.0,
+                weight: 2,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 1.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.Clockwise);
+
+            // Arrange expected
+            var expected = _factory.CreateArc(
+                radius: 1.0,
+                weight: 2,
+                centerX: 6.0,
+                centerY: 6.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 6
+        [Test]
+        public void
+            When_calling_ConvolveArcs_With_same_clockwise_orientation_and_r1_greater_than_r2_Then_the_correct_result_Should_be_returned()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 1,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.Clockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 1.0,
+                weight: 2,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 51.0,
+                orientation: Orientation.Clockwise);
+
+            // Arrange expected
+            var expected = _factory.CreateArc(
+                radius: 3.0,
+                weight: 2,
+                centerX: 6.0,
+                centerY: 6.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 51.0,
+                orientation: Orientation.Clockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 7
+        [Test]
+        public void
+            When_calling_ConvolveArcs_With_opposite_orientation_and_r1_equals_r2_Then_the_correct_result_Should_be_returned()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 5,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 2,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 1.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.Clockwise);
+
+            // Arrange expected
+            var expected = _factory.CreateArc(
+                radius: 0.0,
+                weight: 10,
+                centerX: 6.0,
+                centerY: 6.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 8
+        [Test]
+        public void
+            When_calling_ConvolveArcs_With_same_counterClockwise_orientation_and_r1_smaller_than_r2_Then_the_correct_result_Should_be_returned()
+        {
+            // Arrange arc1
+            var arc1 = _factory.CreateArc(
+                radius: 1.0,
+                weight: 2,
+                centerX: 1.0,
+                centerY: 3.0,
+                directionStartX: 2.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 5.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange arc2
+            var arc2 = _factory.CreateArc(
+                radius: 2.0,
+                weight: 2,
+                centerX: 5.0,
+                centerY: 3.0,
+                directionStartX: 1.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 3.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Arrange expected
+            var expected = _factory.CreateArc(
+                radius: 3.0,
+                weight: 4,
+                centerX: 6.0,
+                centerY: 6.0,
+                directionStartX: 12.0,
+                directionStartY: 0.0,
+                directionEndX: 0.0,
+                directionEndY: 51.0,
+                orientation: Orientation.CounterClockwise);
+
+            // Act
+            var actual = _factory.ConvolveArcs(arc1, arc2);
+
+            // Assert
+            actual.Should().HaveCount(1);
+            actual.Single().Convolution.As<Arc<double>>().Should().BeEquivalentTo(expected);
+        }
+
+        // Case 9
+        [Test]
+        public void When_calling_Convolve_With_two_segment_with_same_orientation_The_result_should_be_correct()
+        {
+            // Arrange segment
+            var segment1 = _factory.CreateSegment(
+                startX: 10,
+                startY: 5,
+                endX: 10,
+                endY: 10,
+                weight: 4);
+
+            // Arrange segment
+            var segment2 = _factory.CreateSegment(
+                startX: -1,
+                startY: 5,
+                endX: -1,
+                endY: 10,
+                weight: 4);
+
+            // Act
+            var actual = _factory.Convolve(segment1, segment2);
+
+            // Assert
+            actual.Should().BeEmpty();
+        }
+
+        // Case 10
+        [Test]
+        public void When_calling_Convolve_With_two_segment_with_opposite_orientation_The_result_should_be_correct()
+        {
+            // Arrange segment
+            var segment1 = _factory.CreateSegment(
+                startX: 10,
+                startY: 5,
+                endX: 10,
+                endY: 10,
+                weight: 4);
+
+            // Arrange segment
+            var segment2 = _factory.CreateSegment(
+                startX: -1,
+                startY: 10,
+                endX: -1,
+                endY: 5,
+                weight: 4);
+
+            // Act
+            var actual = _factory.Convolve(segment1, segment2);
+
+            // Assert
+            actual.Should().BeEmpty();
         }
     }
 }
---
+++ tests/README.md
@@ -0,0 +1,5 @@
+# Unit tests
+
+Here are the shapes and the expected convolution used in the unit tests:
+
+![drawings](Drawings/drawing.svg)
