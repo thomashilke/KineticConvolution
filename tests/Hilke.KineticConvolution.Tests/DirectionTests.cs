feaf94d58742d5422dd70487537beedac2f361b5
Thomas Hilke <thomas.foetisch@gmail.com>
2021-02-05 14:18:10 +0100

    Fix the implementation of Direction.BelongsTo for the range boundaries (#8)
    Co-authored-by: Thomas Hilke <thomas.hilke.ch@gmail.com>

--- tests/Hilke.KineticConvolution.Tests/DirectionTests.cs
+++ tests/Hilke.KineticConvolution.Tests/DirectionTests.cs
@@ -29,5 +29,27 @@ namespace Hilke.KineticConvolution.Tests
             directionInLowerHalfPlan.BelongsTo(lowerHalfPlan).Should().BeTrue();
             directionInLowerHalfPlan.BelongsTo(upperHalfPlan).Should().BeFalse();
         }
+
+        [Test]
+        public void When_Direction_Is_One_DirectionRange_Extremity_Then_Direction_Should_Not_Belongs_Strictly_To_DirectionRange()
+        {
+            var factory = new ConvolutionFactory();
+
+            var east = factory.CreateDirection(1.0, 0.0);
+            var north = factory.CreateDirection(0.0, 1.0);
+
+            var directionRange1 = factory.CreateDirectionRange(east, north, Orientation.CounterClockwise);
+            var directionRange2 = factory.CreateDirectionRange(east, north, Orientation.Clockwise);
+
+            east.BelongsTo(directionRange1).Should().BeTrue();
+            east.BelongsTo(directionRange2).Should().BeTrue();
+            north.BelongsTo(directionRange1).Should().BeTrue();
+            north.BelongsTo(directionRange2).Should().BeTrue();
+
+            east.StrictlyBelongsTo(directionRange1).Should().BeFalse();
+            east.StrictlyBelongsTo(directionRange2).Should().BeFalse();
+            north.StrictlyBelongsTo(directionRange1).Should().BeFalse();
+            north.StrictlyBelongsTo(directionRange2).Should().BeFalse();
+        }
     }
 }
